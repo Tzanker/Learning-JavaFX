f73265949b5294ce7c05ffad9cecf621f110d743
Tzanker <47015099+Tzanker@users.noreply.github.com>
2020-03-19 14:59:51 -0400

    The earth now teleports to the other side of the sun on space

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,8 +1,11 @@
 package sample;
+
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
@@ -16,36 +19,46 @@ import javafx.scene.text.FontPosture;
 import javafx.scene.image.Image;
 
 import java.net.URL;
+import java.util.ArrayList;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle( "Canvas Example" );
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Canvas Example");
 
         Group root = new Group();
-        Scene theScene = new Scene( root );
-        primaryStage.setScene( theScene );
+        Scene theScene = new Scene(root);
+        primaryStage.setScene(theScene);
 
-        Canvas canvas = new Canvas( 500, 500 );
-        root.getChildren().add( canvas );
-
+        Canvas canvas = new Canvas(500, 500);
+        root.getChildren().add(canvas);
+        ArrayList<Integer> input = new ArrayList<Integer>();
+        input.add(1);
+        theScene.setOnKeyPressed(
+                new EventHandler<KeyEvent>() {
+                    public void handle(KeyEvent e) {
+                        String code = e.getCode().toString();
+                        if (code == "SPACE") {
+                            input.set(0,input.get(0)*-1);
+                        }
+                    }
+                }
+        );
         GraphicsContext gc = canvas.getGraphicsContext2D();
         Image earth = new Image(getClass().getResourceAsStream("earth.png"));
         Image sun = new Image(getClass().getResourceAsStream("sun.png"));
 
         final long startNanoTime = System.nanoTime();
-        new AnimationTimer()
-        {
-            public void handle(long currentNanoTime)
-            {
-                double t = (currentNanoTime - startNanoTime)/1000000000.0;
-                double x = 232 + 128 * Math.cos(t);
-                double y = 232 + 128 * Math.sin(t);
-                gc.clearRect(0, 0, 500,500);
-                gc.drawImage( earth, x,y);
+        new AnimationTimer() {
+            public void handle(long currentNanoTime) {
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+                double x = 232 + 128 * Math.cos(t)*input.get(0);
+                double y = 232 + 128 * Math.sin(t*input.get(0));
+                gc.clearRect(0, 0, 500, 500);
+                gc.drawImage(earth, x, y);
                 gc.drawImage(sun, 196, 196);
-        }
+            }
 
         }.start();
         primaryStage.show();
